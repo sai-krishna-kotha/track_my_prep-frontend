f06b2fc9fb93a97591a677834ef27d579fb8fc11
SAI KRISHNA KOTHA <kotha.sai.krishna.in@gmail.com>
2025-09-04 00:14:23 +0530

    fix: refresh token logic implemented

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -17,4 +17,73 @@ apiClient.interceptors.request.use(
   }
 );
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function(resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        }).then(token => {
+          originalRequest.headers['Authorization'] = 'Bearer ' + token;
+          return apiClient(originalRequest);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (!refreshToken) {
+        window.location.href = '/login';
+        return Promise.reject(error);
+      }
+      
+      try {
+        const rs = await axios.post(`${apiClient.defaults.baseURL}/token/refresh/`, {
+          refresh: refreshToken
+        });
+
+        const { access } = rs.data;
+        localStorage.setItem('accessToken', access);
+        apiClient.defaults.headers.common['Authorization'] = 'Bearer ' + access;
+        originalRequest.headers['Authorization'] = 'Bearer ' + access;
+        
+        processQueue(null, access);
+        return apiClient(originalRequest);
+
+      } catch (_error) {
+        processQueue(_error, null);
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login';
+        return Promise.reject(_error);
+
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
